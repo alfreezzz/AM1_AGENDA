24f9dc2887e798ef349363fcb38596678e38d218
AlfrizaAkhmadR <144579015+alfreezzz@users.noreply.github.com>
2025-04-15 09:15:06 +0700

    sasaad

--- vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
@@ -9,15 +9,15 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists($publicPath.$uri)) {
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
     return false;
 }
 
 $formattedDateTime = date('D M j H:i:s Y');
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-$remoteAddress = $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'];
+$remoteAddress = $_SERVER['REMOTE_ADDR'] . ':' . $_SERVER['REMOTE_PORT'];
 
 file_put_contents('php://stdout', "[$formattedDateTime] $remoteAddress [$requestMethod] URI: $uri\n");
 
-require_once $publicPath.'/index.php';
+require_once $publicPath . '/index.php';
