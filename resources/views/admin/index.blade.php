fb406b6f17c20721dd165f83bbe9185a894888a4
syishun <fupaaaa1@gmail.com>
2025-05-14 11:23:58 +0700

    ksdjkug

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -124,7 +124,6 @@
                                         <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
                                 </a>
-                                <span class="text-sm text-gray-500">5 agenda hari ini</span>
                             </div>
                         </div>
                     </div>
@@ -157,7 +156,6 @@
                                         <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
                                 </a>
-                                <span class="text-sm text-gray-500">2 guru tidak hadir</span>
                             </div>
                         </div>
                     </div>
@@ -191,7 +189,6 @@
                                         <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
                                 </a>
-                                <span class="text-sm text-gray-500">8 kelas tercatat</span>
                             </div>
                         </div>
                     </div>
@@ -225,7 +222,6 @@
                                         <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
                                 </a>
-                                <span class="text-sm text-gray-500">12 laporan hari ini</span>
                             </div>
                         </div>
                     </div>
