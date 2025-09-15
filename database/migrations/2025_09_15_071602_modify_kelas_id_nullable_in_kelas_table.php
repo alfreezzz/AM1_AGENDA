5e88195de457ad168e4fd020026b481035f24240
AlfrizaAkhmadR <144579015+alfreezzz@users.noreply.github.com>
2025-09-15 15:13:39 +0700

    fix migrasi

--- database/migrations/2025_09_15_071602_modify_kelas_id_nullable_in_kelas_table.php
+++ database/migrations/2025_09_15_071602_modify_kelas_id_nullable_in_kelas_table.php
@@ -1,26 +1,28 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-/**
-* Run the migrations.
-*/
-public function up(): void
-{
-Schema::table('kelas', function (Blueprint $table) {
-$table->string('kelas_id')->nullable()->change();
-});
-}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('kelas', function (Blueprint $table) {
+            $table->string('kelas_id')->nullable()->change();
+        });
+    }
 
-/**
-* Reverse the migrations.
-*/
-public function down(): void
-{
-Schema::table('kelas', function (Blueprint $table) {
-$table->string('kelas_id')->nullable(false)->change();
-});
-}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('kelas', function (Blueprint $table) {
+            $table->string('kelas_id')->nullable(false)->change();
+        });
+    }
 };
